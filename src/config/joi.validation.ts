725416b2133f54e5813aa1a305755ceebf06397c
Jose Juan Garcia <jose.juan.garcia011@gmail.com>
2025-05-08 10:36:07 -0600

    quinto  despliegue

--- src/config/joi.validation.ts
+++
@@ -1,9 +0,0 @@
-
-import * as Joi from  'joi' ;
-
-
-export const JoiValidationSchema =  Joi.object({
-    MONGODB: Joi.required(),
-    PORT: Joi.number().default(3005),
-    DEFAUL_LIMIT: Joi.number().default(6),
-}); 
